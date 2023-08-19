238e867e39c1584f6cb208bd8123535f0157b4f2
Leticia Speda <leticiaspeda@gmail.com>
2023-08-19 16:02:58 -0300

    desafio para fixacao dos exercicios

--- exercicios_java/src/exercicio5.java
+++ exercicios_java/src/exercicio5.java
@@ -1,12 +1,12 @@
 public class exercicio5 {
     public static void main (String[] args) throws Exception {
-        int a,b,B,d,h;
+        int a,b,B,h;
 
         b = 6;
         B = 8;
         h = 5;
 
-        a = (b + B) / 2 * 5 ;
+        a = (b + B) / 2 * h ;
 
         System.out.println(a);
     }
