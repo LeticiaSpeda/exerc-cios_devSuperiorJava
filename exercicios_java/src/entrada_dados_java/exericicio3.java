b7905634f2f2508fa7039b86b6e7780e63285c10
Leticia Speda <leticiaspeda@gmail.com>
2023-08-21 15:54:23 -0300

    chore: adicionado exercicio sobre leitura de um ponto flutuante

---
+++ exercicios_java/src/entrada_dados_java/exericicio3.java
@@ -0,0 +1,19 @@
+package entrada_dados_java;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exericicio3 {
+    
+    public static void main(String[] args) throws Exception {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);    
+    
+    double x;
+
+    x = sc.nextDouble();
+
+    System.out.println("Você digitou: " + x);
+    sc.close();
+    }
+}
