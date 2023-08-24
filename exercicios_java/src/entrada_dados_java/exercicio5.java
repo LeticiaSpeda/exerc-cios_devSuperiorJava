6c96fd9c53081e34cf44d341dfe1550ecd58dd9c
Leticia Speda <leticiaspeda@gmail.com>
2023-08-24 10:39:31 -0300

    chore: adicionado exercicio de leitura em uma unica linha

---
+++ exercicios_java/src/entrada_dados_java/exercicio5.java
@@ -0,0 +1,26 @@
+package entrada_dados_java;
+
+import java.util.Scanner;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String x;
+        int y;
+        double z;
+
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        
+        System.out.println("Você digitou:");
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        sc.close();
+    }
+}
